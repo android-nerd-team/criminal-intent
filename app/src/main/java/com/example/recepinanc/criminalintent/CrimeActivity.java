ef2a97f0590d23aaef51d18e31e70d628c78ca18
recepinanc <recepinanc96@gmail.com>
2015-07-31 20:28:15 +0300

    Fragment works

--- app/src/main/java/com/example/recepinanc/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/recepinanc/criminalintent/CrimeActivity.java
@@ -1,10 +1,17 @@
 package com.example.recepinanc.criminalintent;
 
+
+import android.support.v4.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
 
 
 public class CrimeActivity extends FragmentActivity {
@@ -15,7 +22,8 @@ public class CrimeActivity extends FragmentActivity {
         setContentView(R.layout.activity_crime);
 
 
-
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment 
 
     }
 }
