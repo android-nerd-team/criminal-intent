cd071a2bafca276e7811f79d036d10ad301aa049
Faruk <farukcankayaaa@gmail.com>
2015-08-03 07:24:45 +0300

    Adding fragmennt

--- app/src/main/java/com/example/recepinanc/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/recepinanc/criminalintent/CrimeActivity.java
@@ -1,21 +1,27 @@
 package com.example.recepinanc.criminalintent;
 
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
 
 public class CrimeActivity extends FragmentActivity {
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+    private CrimeFragment crimeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        crimeFragment = new CrimeFragment();
 
-
-
-
+        // Adds Fragment
+        fragmentTransaction.add(R.id.fragmentContainer, crimeFragment)
+                .show(crimeFragment)
+                .commit();
     }
 }
--- app/src/main/java/com/example/recepinanc/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/recepinanc/criminalintent/CrimeFragment.java
@@ -1,16 +1,31 @@
 package com.example.recepinanc.criminalintent;
 
-import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * Created by Recepinanc on 30.07.2015.
  */
 public class CrimeFragment extends Fragment {
-    private Crime mCrime;
+    private Crime crime;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mCrime = new Crime();
+        crime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_crime, container, false);
+        EditText crimeTitle = (EditText) rootView.findViewById(R.id.crime_title);
+
+        // TODO:
+
+        return rootView;
     }
 }
